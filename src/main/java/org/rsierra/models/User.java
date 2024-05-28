0b4573325ed3664f8c3f4f962710d13c8bec8ef3
Raul Sierra <j.raulsierra@gmail.com>
2024-05-28 11:31:15 -0600

    feat(section 10): Model and Repository of User and Profile

---
+++ src/main/java/org/rsierra/models/User.java
@@ -0,0 +1,79 @@
+package org.rsierra.models;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "User")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String username;
+    private String password;
+    private String email;
+    private Integer status;
+    private Date registerDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getRegisterDate() {
+        return registerDate;
+    }
+
+    public void setRegisterDate(Date registerDate) {
+        this.registerDate = registerDate;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", status=" + status +
+                ", registerDate=" + registerDate +
+                '}';
+    }
+}
---
+++ src/main/java/org/rsierra/repository/Profile.java
@@ -0,0 +1,7 @@
+package org.rsierra.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Profile extends JpaRepository<Profile, Integer> {
+}
